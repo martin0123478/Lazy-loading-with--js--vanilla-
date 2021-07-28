0ea2beadf83d26f7eb66998094ea0f5ad302d0a3
martin0123478 <martin_agui97@outlook.es>
2021-07-27 21:00:08 -0500

    se inicia a crear un template y crear elementos html con js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,21 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+//crear imagen
+//agregar #imagen
+const random = () => Math.floor( Math.random() * (maximo - minimo) ) + minimo
+ 
+const createImageNode = () =>{
+  const container = document.createElement('div')
+  container.className = 'p-4'
+  const image = document.createElement('img')
+  image.className = "mx-auto"
+  image.width = '320'
+  image.src = "https://randomfox.ca/images/2.jpg"
+  container.appendChild(image)
+  return container;
+}
+
+const nuevaImagen = createImageNode();
+const mountNode =  document.getElementById('images')
+mountNode.appendChild(nuevaImagen)
